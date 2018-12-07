3186ebdb49eb07710940d9e9ae0c7e8d71476ac3
JulianCano <juliancano.dev@gmail.com>
2018-12-07 11:02:20 -0500

    Validando formulario con un pluggin jquery

--- js/main.js
+++ js/main.js
@@ -144,4 +144,18 @@ $(document).ready(function(){
 
     }
 
+    //Validation
+    if(window.location.href.indexOf('contact') > -1){
+
+        $("form input[name='date']").datepicker({
+            dateFormat: 'dd-mm-yy'
+        });
+
+        $.validate({
+            lang: 'es',
+            errorMessagePosition: 'top',
+            scrollToTopOnError: true
+        });
+    }
+
 });
